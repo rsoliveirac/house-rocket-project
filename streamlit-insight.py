a112218676832ec32df81fb2ae692190593a7db2
rsoliveirac <71949871+rsoliveirac@users.noreply.github.com>
2021-03-14 14:32:56 -0300

    Update streamlit-insight.py

--- streamlit-insight.py
+++ streamlit-insight.py
@@ -14,7 +14,7 @@ def get_data(path):
 
     return df
 
-path = 'https://github.com/rsoliveirac/projeto-insights-houses/blob/3f373d0008d7d173009a4c769bdb5fd7252dd4cb/kc_house_data.csv'
+path = 'kc_data_house.csv'
 df = get_data(path)
 
 #=========================================
