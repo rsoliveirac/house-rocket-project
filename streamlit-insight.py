2c18162d7ab7fb8b2d1336ed050bfe6ca3d0d8b3
rsoliveirac <71949871+rsoliveirac@users.noreply.github.com>
2021-03-14 13:52:48 -0300

    Update streamlit-insight.py

--- streamlit-insight.py
+++ streamlit-insight.py
@@ -14,7 +14,7 @@ def get_data(path):
 
     return df
 
-path = '/home/user/repos/projeto-insight/kc_house_data.csv'
+path = 'projeto-insight/kc_house_data.csv'
 df = get_data(path)
 
 #=========================================
